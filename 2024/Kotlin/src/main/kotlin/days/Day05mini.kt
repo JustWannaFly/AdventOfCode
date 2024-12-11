d947da351378eb98d37b9bf518ded15daeca6baa
JustWannaFly <joshua.korver@datasite.com>
2024-12-11 10:11:59 -0600

    small variable names

--- 2024/Kotlin/src/main/kotlin/days/Day05mini.kt
+++ 2024/Kotlin/src/main/kotlin/days/Day05mini.kt
@@ -2,10 +2,10 @@ package com.github.justwannafly.days
 import java.io.File
 fun main() {
 val i= File("5").readLines()
-val orderPairs=i.filter{it.contains('|')}.map{ listOf(it.split('|')[0], it.split('|')[1]) }
-val updatePages=i.filter{it.contains(',')}.map{it.split(',')}
-fun List<String>.before(page: String)=orderPairs.filter{it[1]==page&&it[0]in this}.map{it[0]}
-fun isRightOrder(pages: List<String>):Boolean{return orderPairs.all{!(pages.contains(it[0])&&pages.contains(it[1])&&pages.indexOf(it[0])>pages.indexOf(it[1]))}}
-println(updatePages.filter{isRightOrder(it)}.sumOf{it[(it.size/2)].toInt()})
-println(updatePages.filter{!isRightOrder(it)}.map{it.sortedBy{page->it.before(page).size}}.sumOf{it[(it.size/2)].toInt()})
+val o=i.filter{it.contains('|')}.map{ listOf(it.split('|')[0], it.split('|')[1]) }
+val p=i.filter{it.contains(',')}.map{it.split(',')}
+fun List<String>.before(a: String)=o.filter{it[1]==a&&it[0]in this}.map{it[0]}
+fun c(a: List<String>):Boolean{return o.all{!(a.contains(it[0])&&a.contains(it[1])&&a.indexOf(it[0])>a.indexOf(it[1]))}}
+println(p.filter{c(it)}.sumOf{it[(it.size/2)].toInt()})
+println(p.filter{!c(it)}.map{it.sortedBy{ page->it.before(page).size}}.sumOf{it[(it.size/2)].toInt()})
 }
